695a89fd43f64877cda4255d766dcb9ae8fcb6fa
Antonio <aortega@creatuviaje.com>
2018-12-20 17:53:04 -0600

    first commit, some components created and App style added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Button from './Components/Button'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #711400;
+`
+
+const Content = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: white;
+  box-shadow: 1px 1px 4px black;
+  border-radius: 5px;
+`
+
+const Header = styled.header`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+`
+
+const Score = styled.div`
+  flex-grow: 1;
+  display:flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 20px;
+`
+
+const TitleScore = styled.div`
+  color: #951B00;
+`
+
+const Instruction = styled.div`
+  flex-grow: 2;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+
+const Paragraph = styled.div`
+  
+`
+
 
 class App extends Component {
   render() {
+    const  options =  ['Piedra', 'Papel','Tijera'];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Content>
+          <Header>
+            <Instruction>
+                <label>asdf</label>
+                <label>asdf</label>
+            </Instruction>
+            <Score>
+              <TitleScore>Score</TitleScore>
+              <label>asdf</label>
+            </Score>
+          </Header>
+        </Content>
+      </Container>
     );
   }
 }
---
+++ src/Components/Button/index.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+
+function Button(props){
+
+};
+
+export default Button;
