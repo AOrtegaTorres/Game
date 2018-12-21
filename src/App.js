efd4dc42a7060ebfd69c057c4f74376c469f0d3d
Antonio <aortega@creatuviaje.com>
2018-12-20 18:35:58 -0600

    button component finished

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   background-color: #711400;
-`
+`;
 
 const Content = styled.div`
   width: 50%;
@@ -17,14 +17,14 @@ const Content = styled.div`
   background-color: white;
   box-shadow: 1px 1px 4px black;
   border-radius: 5px;
-`
+`;
 
 const Header = styled.header`
   width: 100%;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
-`
+`;
 
 const Score = styled.div`
   flex-grow: 1;
@@ -33,29 +33,41 @@ const Score = styled.div`
   justify-content: center;
   align-items: center;
   padding: 20px;
-`
+`;
 
 const TitleScore = styled.span`
   color: #951B00;
   font-size: 30px;
-`
+`;
 
 const Players = styled.span`
   color: black;
-`
+`;
 
 const Instruction = styled.div`
   flex-grow: 2;
   display: flex;
   flex-direction: column;
   padding: 20px;
-`
+`;
 
 const Paragraph = styled.div`
+  width: 100%;
   color:#A7A6A6;
   font-size: 30px;
-`
+`;
 
+const Options = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+`;
+
+const Result = styled.h1`
+  font-size: 30px;
+  color: black;
+`
 
 class App extends Component {
   constructor(){
@@ -65,11 +77,15 @@ class App extends Component {
         pc:0,
         player:0
       },
-      options:['Piedra', 'Papel','Tijera']
+      options:[{'text':'Piedra', 'color': '#CCCCCC'}, {'text':'Papel', 'color':'#FFFFFF'},{'text':'Tijera','color':'#5F92EA'}],
     }
   }
+
+  handleClick(element){
+    const choice = element.target.value;
+  }
   render() {
-    const { player, pc } = this.state.score
+    const { score :{ player, pc }, options } = this.state
     return (
       <Container>
         <Content>
@@ -83,6 +99,14 @@ class App extends Component {
               <Players>{`PC: ${pc}`}</Players>
             </Score>
           </Header>
+          <Options>
+            <Paragraph>Elije tu jugada</Paragraph>
+            {options.map((option,i) => {
+              return(
+                <Button key={`button${i}`} color={option.color} text={option.text} handleClick={this.handleClick} />)}
+              )
+            }
+          </Options>
         </Content>
       </Container>
     );
--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,8 +1,29 @@
-import React,{Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-function Button(props){
+const ButtonComponent = styled.button`
+  width: 20%;
+  height: 50px;
+  background-color: ${props => props.color };
+  border:none;
+  box-shadow: 1px 1px 2px black;
+  margin-top: 20px;
+  font-size: 20px;
+`;
 
+
+function Button(props){
+  return(
+    <ButtonComponent color={props.color} value={props.text} onClick={(element)=>props.handleClick(element)}>
+      {props.text}
+    </ButtonComponent>
+  )
 };
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string
+}
 
 export default Button;
